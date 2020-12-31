ae80e85114c8bf27e0f1215c33ff56f07a4acb98
freitassp <priettosamuel@gmail.com>
2020-12-31 11:39:34 -0300

    Seletor nas tag e aquivo js externo

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+console.log("Iniciando de um arquivo externo");
+let titulo = document.querySelector(".titulo");
+		
+        titulo.textContent = "Engenharia de Software";
+        console.log(titulo);
