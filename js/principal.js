f8a02c7ed9012dd585b2b744e70f0e95f92a0475
freitassp <priettosamuel@gmail.com>
2021-01-01 22:08:49 -0300

    querySelector e textContent

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,28 @@
-console.log("Iniciando de um arquivo externo");
-let titulo = document.querySelector(".titulo");
-		
-        titulo.textContent = "Engenharia de Software";
-        console.log(titulo);
+var titulo = document.querySelector(".titulo");
+    
+ var primeiroPaciente = document.querySelector("#primeiro-paciente");
+
+ var tdPeso = primeiroPaciente.querySelector(".info-peso");
+ var peso = tdPeso.textContent;
+
+ var tdAltura = primeiroPaciente.querySelector(".info-altura");
+ var altura = tdAltura.textContent;
+
+
+ var tdImc = primeiroPaciente.querySelector(".info-imc");
+ var imc = peso/ (altura*altura);
+     tdImc.textContent = imc
+
+     
+     
+
+ console.log(primeiroPaciente);//tr
+ console.log(tdPeso);//td q contem peso
+ console.log(peso);//conteudo da td
+ console.log(altura);//conteudo td altura
+ console.log(imc)
+
+ 
+ 
+
+       
