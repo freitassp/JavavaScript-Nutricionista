d6467ceb0e7dc1a0e0ea63e72ee7fe55821f5e47
freitassp <priettosamuel@gmail.com>
2021-01-05 20:53:57 -0300

    createElement and appendChild()

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,6 @@
 
-
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Sistema Nutricionista";
     
  var pacientes = document.querySelectorAll(".paciente");
  console.log(pacientes);
@@ -44,3 +45,47 @@ if(altura <= 0 || altura >= 3.00){
  
 
 }       
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(event){
+        event.preventDefault();
+        var form = document.querySelector("#form-adiciona");
+        
+        var nome  = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+        var pacienteTr = document.createElement("tr");
+       
+
+        var nomeTd = document.createElement("td"); 
+        var pesoTd = document.createElement("td"); 
+        var alturaTd = document.createElement("td"); 
+        var gorduraTd = document.createElement("td"); 
+        var imcTd = document.createElement("td"); 
+
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+
+
+        pacienteTr.appendChild(nomeTd); // anexar filho na mae
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+       
+
+       
+       
+        
+});
+
+
+
+
+
+
+
+
