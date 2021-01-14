997751625fb16fa73d244c0874ceb260cea95afe
freitassp <priettosamuel@gmail.com>
2021-01-14 06:27:00 -0300

    commit

--- js/form.js
+++ js/form.js
@@ -7,10 +7,19 @@ botaoAdicionar.addEventListener("click",function(event){
     var paciente = obtemPacienteFormulario(form);
 
        //cria a tr e a td do pacientehg
+<<<<<<< HEAD
     var pacienteTr =  montaTr(paciente);
         //adicionando paciente na tabela
      var tabela =document.querySelector("#tabela-pacientes");
          tabela.appendChild(pacienteTr);
+=======
+        var pacienteTr = montaTr(paciente);
+
+        //adicionando paciente na tabela
+        var tabela =document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
+        form.reset();
+>>>>>>> b504c2d9c20b9091b38bba9a8d13f5419eee60ea
        
        //lompando campos formulario
     form.reset();  
@@ -35,6 +44,7 @@ function montaTr(paciente){
 
     var pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
+<<<<<<< HEAD
 
     pacienteTr.appendChild(montaTd(paciente.nome,"info-nome")); // anexar filho na mae
     pacienteTr.appendChild(montaTd(paciente.peso,"info-peso"));
@@ -51,6 +61,21 @@ function montaTd(dado,classe){
     td.textContent = dado;
     td.classList.add(classe);
 
+=======
+    
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+}
+
+function montaTd(dado, classe){
+    var td = createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+>>>>>>> b504c2d9c20b9091b38bba9a8d13f5419eee60ea
     return td;
 
 }
