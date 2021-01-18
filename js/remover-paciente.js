339cf1c425937fffe4c0ba4d43b82d1dd6898b8e
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2021-01-18 12:31:09 -0300

    commit

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,5 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
 pacientes.forEach(function(paciente){
-    paciente.addEventListener()
+    paciente.addEventListener("dblclick", function(){
+        console.log("Fui clicado com duplo clique");
+        this.remove();
+
+    });
 
 });
