b32c902a629a0af6c94e83053e853b85f612af39
freitassp <priettosamuel@gmail.com>
2021-01-24 11:25:51 -0300

    removendo o evento alvo do duplo click

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,15 @@
 var pacientes = document.querySelectorAll(".paciente");
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado com duplo clique");
-        this.remove();
 
-    });
-
+var tabela = document.querySelector("#tabela-pacientes")
+tabela.addEventListener("dblclick", function(event){
+console.log(event.target);
+event.target.parentNode.remove(); // evento.  alvo(onde cliquei) . pai do alvo . remove
+    // this.remove(event.target);
 });
+// pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+//        console.log("Fui clicado com duplo clique");
+//        this.remove();
+
+//     });
+
